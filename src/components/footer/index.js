4f8c35287a9712198bbb10904e7aec1d70491360
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-04-13 23:56:15 +0500

    Fix bugs

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -10,16 +10,16 @@ import tw from "../../assets/img/twitter.svg";
 import lin from "../../assets/img/linkedin.svg";
 import ins from "../../assets/img/instagram.svg";
 
-import loc from '../../assets/img/lokatsya.svg';
-import mail from '../../assets/img/letter.svg';
-import phone from '../../assets/img/tel.svg';
+import loc from "../../assets/img/lokatsya.svg";
+import mail from "../../assets/img/letter.svg";
+import phone from "../../assets/img/tel.svg";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="container d-flex justify-content-between">
         <div className="footer__item">
-          <Link to="/" href="#" className="header__logo">
+          <Link to="/" href="http" className="header__logo">
             <img src={logo} alt="" />
             <span className="header__logo--right">
               <span className="header__logo--name">Pir.uz</span>
@@ -36,82 +36,119 @@ const Footer = () => {
           <div className="footer__title">Bizni kuzating</div>
           <ul className="footer__social">
             <li>
-              <a href="#" className="fb">
+              <a href="http" className="fb">
                 <img src={fb} alt="" />
               </a>
             </li>
             <li>
-              <a href="#" className="yt">
+              <a href="http" className="yt">
                 <img src={yt} alt="" />
               </a>
             </li>
             <li>
-              <a href="#" className="tw">
+              <a href="http" className="tw">
                 <img src={tw} alt="" />
               </a>
             </li>
             <li>
-              <a href="#" className="lin">
+              <a href="http" className="lin">
                 <img src={lin} alt="" />
               </a>
             </li>
             <li>
-              <a href="#" className="ins">
+              <a href="http" className="ins">
                 <img src={ins} alt="" />
               </a>
             </li>
           </ul>
         </div>
         <div className="footer__item">
-            <div className="footer__title">Kitoblar janrlari</div>
-            <ul className="footer__menu half">
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-            </ul>
+          <div className="footer__title">Kitoblar janrlari</div>
+          <ul className="footer__menu half">
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+          </ul>
         </div>
         <div className="footer__item">
-            <div className="footer__title">Tezkor havolalar</div>
-            <ul className="footer__menu">
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-                <li><a href="#">Roman</a></li>
-            </ul>
+          <div className="footer__title">Tezkor havolalar</div>
+          <ul className="footer__menu">
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+            <li>
+              <a href="http">Roman</a>
+            </li>
+          </ul>
         </div>
         <div className="footer__item">
-            <div className="footer__title">Bizning do’kon</div>
-            <div className="footer_map"></div>
-            <div className="footer__contact">
-                <img src={loc} alt="" />
-                <div className="footer__val">27 Chashtepa ko'chasi, <br />
-                Toshkent, O`zbekiston</div>
+          <div className="footer__title">Bizning do’kon</div>
+          <div className="footer_map"></div>
+          <div className="footer__contact">
+            <img src={loc} alt="" />
+            <div className="footer__val">
+              27 Chashtepa ko'chasi, <br />
+              Toshkent, O`zbekiston
             </div>
-            <div className="footer__contact">
-                <img src={phone} alt="" />
-                <div className="footer__val">+998555004466</div>
-            </div>
-            <div className="footer__contact">
-                <img src={mail} alt="" />
-                <div className="footer__val">piruzofficial@gmail.com
-            </div>
-            </div>
+          </div>
+          <div className="footer__contact">
+            <img src={phone} alt="" />
+            <div className="footer__val">+998555004466</div>
+          </div>
+          <div className="footer__contact">
+            <img src={mail} alt="" />
+            <div className="footer__val">piruzofficial@gmail.com</div>
+          </div>
         </div>
       </div>
       <div className="footer__bottom">
-            <div className="container">
-                    <div className="footer__copy">Pir.uz  -   © 2022 All Rights Reserved</div>
-                    <a href="#" className="footer__author">Made with ♥ by Doin.uz</a>
-            </div>
+        <div className="container">
+          <div className="footer__copy">
+            Pir.uz - © 2022 All Rights Reserved
+          </div>
+          <a href="http" className="footer__author">
+            Made with ♥ by Doin.uz
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -12,8 +12,7 @@ import user from "../../assets/img/user.svg";
 import down from "../../assets/img/down.svg";
 
 const Header = () => {
-
-  const [catBtn,setCatbtn] = useState(false);
+  const [catBtn, setCatbtn] = useState(false);
 
   return (
     <header className="header">
@@ -26,18 +25,35 @@ const Header = () => {
           </span>
         </Link>
         <div className="header__search">
-          <button className={catBtn?"header__menu btn show":'header__menu btn'} onClick={()=>{setCatbtn(!catBtn)}}>
+          <button
+            className={catBtn ? "header__menu btn show" : "header__menu btn"}
+            onClick={() => {
+              setCatbtn(!catBtn);
+            }}
+          >
             <img src={menu} alt="" />
             <span>Category</span>
             <img src={down} alt="" />
             <ul className="cats">
-              <li><Link to='/'>Roman</Link></li>
-              <li><Link to='/'>Roman</Link></li>
-              <li><Link to='/'>Roman</Link></li>
-              <li><Link to='/'>Roman</Link></li>
-              <li><Link to='/'>Roman</Link></li>
-              <li><Link to='/'>Roman</Link></li>
-          </ul>
+              <li>
+                <Link to="/">Roman</Link>
+              </li>
+              <li>
+                <Link to="/">Roman</Link>
+              </li>
+              <li>
+                <Link to="/">Roman</Link>
+              </li>
+              <li>
+                <Link to="/">Roman</Link>
+              </li>
+              <li>
+                <Link to="/">Roman</Link>
+              </li>
+              <li>
+                <Link to="/">Roman</Link>
+              </li>
+            </ul>
           </button>
           <input type="text" placeholder="Search" className="header__input" />
           <button className="header__find btn">
